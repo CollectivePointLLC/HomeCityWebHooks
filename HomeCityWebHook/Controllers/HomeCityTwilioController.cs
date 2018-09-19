55765d79faeb6fc75177b0298cb52574b05deb9b
Todd Allen <todd@collectivepoint.com>
2018-09-19 09:52:11 -0500

    initial commit

---
+++ HomeCityWebHook/Controllers/HomeCityTwilioController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Twilio.AspNet.Common;
+using Twilio.AspNet.Mvc;
+using Twilio.TwiML;
+using System.Data.SqlClient;
+using System.ComponentModel;
+
+namespace HomeCityTwilioWebHook.Controllers
+{
+    public class HomeCityTwilioController : TwilioController
+    {
+        [HttpPost]
+        public TwiMLResult Index(SmsRequest request)
+        {
+
+            var result = new List<string>();
+            foreach (PropertyDescriptor property in TypeDescriptor.GetProperties(request))
+            {
+                result.Add(property.Name + "=" + property.GetValue(request));
+            }
+
+            string newResult = string.Join("&", result);
+
+            string cnnString = System.Configuration.ConfigurationManager.ConnectionStrings["HomeCityDatabase"].ConnectionString;
+
+            SqlConnection cnn = new SqlConnection(cnnString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cnn;
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.CommandText = "usp_tw_message_manage";
+            //add any parameters the stored procedure might require
+            cmd.Parameters.Add(new SqlParameter("@action", "insert"));
+            cmd.Parameters.Add(new SqlParameter("@response", newResult));
+
+            cnn.Open();
+
+            object o = cmd.ExecuteScalar();
+
+            cnn.Close();
+
+
+            var response = new Twilio.TwiML.MessagingResponse();
+            //response.Message("This is my first message");
+            
+            return TwiML(response);
+        }
+
+        
+    }
+}
